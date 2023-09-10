445b30e62e0dd1a888addff901095fb0b13e7ff2
Zai <zaiyellyintaung@gmail.com>
2023-09-10 23:38:37 +0630

    project setup

---
+++ alpha/app/layout.tsx
@@ -0,0 +1,28 @@
+import "@/styles/globals.css"
+import { Metadata } from "next"
+import { Inter } from "next/font/google"
+
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  )
+}
