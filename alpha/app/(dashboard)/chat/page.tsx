50f601bb8bc95428dcec8790f08f63d42c4421d1
Zai <zaiyellyintaung@gmail.com>
2023-09-12 03:18:02 +0630

    done input

--- alpha/app/(dashboard)/chat/page.tsx
+++ alpha/app/(dashboard)/chat/page.tsx
@@ -1,8 +1,31 @@
-import { MessageSquare } from "lucide-react"
+"use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { MessageSquare } from "lucide-react"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import Header from "@/components/main/Header"
 
+import { formSchema } from "./constants"
+
 export default function ChatPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  })
+
+  const isLoading = form.formState.isSubmitting
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
   return (
     <main>
       {" "}
@@ -13,6 +36,51 @@ export default function ChatPage() {
         iconColor="text-violet-500"
         bgColor="bg-violet-500/10"
       />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none shadow-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="How do I calculate the radius of a circle?"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                type="submit"
+                disabled={isLoading}
+                size="icon"
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
     </main>
   )
 }
