b6be2dee1adfa9f1fd35a3303b1f72cdfc5a7ca8
Zai <zaiyellyintaung@gmail.com>
2023-09-12 18:44:41 +0630

    only image generation left

--- alpha/app/(dashboard)/code/page.tsx
+++ alpha/app/(dashboard)/code/page.tsx
@@ -1,8 +1,54 @@
-import { Code } from "lucide-react"
+"use client"
 
+import { useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import axios from "axios"
+import { Code, MessageSquare } from "lucide-react"
+import { ChatCompletionMessage } from "openai/resources/chat"
+import { useForm } from "react-hook-form"
+import ReactMarkdown from "react-markdown"
+import * as z from "zod"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { BotAvatar, UserAvatar } from "@/components/main/Avatars"
 import Header from "@/components/main/Header"
+import Loading from "@/components/main/Loading"
+import None from "@/components/main/None"
+
+import { formSchema } from "./constants"
 
 export default function CodePage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  })
+
+  const isLoading = form.formState.isSubmitting
+
+  const [messages, setMessages] = useState<ChatCompletionMessage[]>([])
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const userMessage: ChatCompletionMessage = {
+        role: "user",
+        content: values.prompt,
+      }
+      const newMessages = [...messages, userMessage]
+
+      const response = await axios.post("/api/code", {
+        prompt: newMessages,
+      })
+
+      setMessages((current) => [...current, userMessage, response.data])
+      form.reset()
+    } catch (err) {}
+  }
+
   return (
     <main>
       {" "}
@@ -13,6 +59,89 @@ export default function CodePage() {
         iconColor="text-green-700"
         bgColor="bg-green-700/10"
       />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none shadow-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="How do I calculate the radius of a circle?"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                type="submit"
+                disabled={isLoading}
+                size="icon"
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 my-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loading />
+            </div>
+          )}
+          {!isLoading && messages.length === 0 && (
+            <None label="No conversation started." />
+          )}
+        </div>
+        <div className="flex flex-col-reverse gap-y-4">
+          {messages.map((message) => (
+            <div
+              key={message.content}
+              className={cn(
+                "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                message.role === "user" ? " border border-black/10" : "bg-muted"
+              )}
+            >
+              {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+              <ReactMarkdown
+                components={{
+                  pre: ({ node, ...props }) => (
+                    <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                      <pre {...props} />
+                    </div>
+                  ),
+                  code: ({ node, ...props }) => (
+                    <code className="bg-black/10 rounded-lg p-1" {...props} />
+                  ),
+                }}
+                className="text-sm overflow-hidden leading-7"
+              >
+                {message.content || ""}
+              </ReactMarkdown>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
